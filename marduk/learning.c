6ec92fabb29cf48411e579513b8cb52903541996
Marduk <lucasrafael.exe@gmail.com>
2025-05-08 14:30:30 -0300

    Arquivo learning.c modificado
    Removi parte do código que não tinha relação com a criação da pirâmide, e coloquei no novo arquivo chamado numbers.c

--- marduk/learning.c
+++ marduk/learning.c
@@ -1,28 +1,8 @@
 #include <stdio.h>
 
-#define MAX 10
 #define BRICKS 8
 
 int main(void) {
-	/*int value = 5;
-	
-	for (int i = 0; i <= MAX; i++) {
-		int product = value * i;
-		printf("%d x %d = %d\n", value, i, product);
-	}
-	
-	printf("\n");
-	
-	for (int i = 0; i < BRICKS; i++) {
-		for (int j = 0; j < BRICKS; j++) {
-			if (i >= j) printf("#");
-			else printf(" ");
-		}
-		printf("\n");
-	}
-	
-	printf("\n");*/
-	
 	int k = BRICKS-1;
 	
 	for (int i = 0; i < BRICKS; i++) {
