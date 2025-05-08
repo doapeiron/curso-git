79deec542cae5ebd37a6ddb2861003657bb47ddf
Marduk <lucasrafael.exe@gmail.com>
2025-05-08 14:31:51 -0300

    Arquivo numbers.c criado

---
+++ marduk/numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define MAX 10
+
+int main(void) {
+	int value = 5;
+		
+		for (int i = 0; i <= MAX; i++) {
+			int product = value * i;
+			printf("%d x %d = %d\n", value, i, product);
+		}
+	
+	return 0;
+}
